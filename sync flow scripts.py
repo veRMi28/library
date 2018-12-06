81bca76fe00450110ef518bd6252096153a18c1a
Stefan Mückstein <stefan@cloudomation.io>
2018-12-06 14:43:41 +0100

    also sync yaml files as settings

--- sync flow scripts.py
+++ sync flow scripts.py
@@ -39,4 +39,22 @@ def handler(c):
             json=flow_dict,
             pass_user_token=True,
         ).runAsync()
+    settings = c.list_dir('flows', '**/*.yaml')
+    c.setOutput('settings', settings)
+    for setting in settings:
+        content = c.file(f'flows/{setting}')
+        name = setting[:-5]
+        setting_dict = {
+            'name': name,
+            'variable_value': content,
+        }
+        # update the setting in cloudomation. the API will automatically
+        # fall back to POST (creating a record) when the record is not found.
+        c.task(
+            'REST',
+            url=f'https://{env_name}.cloudomation.io/api/1/setting/{name}',
+            method='PATCH',
+            json=setting_dict,
+            pass_user_token=True,
+        ).runAsync()
     c.success(message='all done')
