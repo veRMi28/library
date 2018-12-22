5ab68c8e7d5cf112378c84213b8146c1d9c38f49
Stefan Mückstein <stefan@cloudomation.io>
2018-12-22 09:42:44 +0100

    settings and flows in subdirectories, quote names in urls

--- sync flow scripts.py
+++ sync flow scripts.py
@@ -1,3 +1,6 @@
+import urllib
+
+
 def handler(c):
     env_name = c.getEnvName()
     inputs = c.getInputs()
@@ -16,16 +19,17 @@ def handler(c):
         'GIT',
         command='get',
         repository_url=repo_info['repository_url'],
-        repository_path=repo_info['repository_path'],
+        repository_path='repo',
         httpCookie=repo_info['httpCookie'],
         ref=commit_sha,
     ).run()
     # list all flows from the repository
-    flows = c.list_dir('flows', glob='**/*.py')
+    flows = c.list_dir('repo/flows', glob='**/*.py')
     c.setOutput('flows', flows)
     for flow in flows:
-        content = c.file(f'flows/{flow}')
+        content = c.file(f'repo/flows/{flow}')
         name = flow[:-3]
+        urlname = urllib.parse.quote(name)
         flow_dict = {
             'name': name,
             'script': content,
@@ -34,16 +38,17 @@ def handler(c):
         # fall back to POST (creating a record) when the record is not found.
         c.task(
             'REST',
-            url=f'https://{env_name}.cloudomation.io/api/1/flow/{name}',
+            url=f'https://{env_name}.cloudomation.io/api/1/flow/{urlname}',
             method='PATCH',
             json=flow_dict,
             pass_user_token=True,
         ).runAsync()
-    settings = c.list_dir('flows', '**/*.yaml')
+    settings = c.list_dir('repo/settings', '**/*.yaml')
     c.setOutput('settings', settings)
     for setting in settings:
-        content = c.file(f'flows/{setting}')
+        content = c.file(f'repo/settings/{setting}')
         name = setting[:-5]
+        urlname = urllib.parse.quote(name)
         setting_dict = {
             'name': name,
             'value': content,
@@ -52,7 +57,7 @@ def handler(c):
         # fall back to POST (creating a record) when the record is not found.
         c.task(
             'REST',
-            url=f'https://{env_name}.cloudomation.io/api/1/setting/{name}',
+            url=f'https://{env_name}.cloudomation.io/api/1/setting/{urlname}',
             method='PATCH',
             json=setting_dict,
             pass_user_token=True,
