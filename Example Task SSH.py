c7450e9c7f667408b66055d0bf35139c51333055
Stefan Mückstein <stefan@cloudomation.io>
2019-03-26 10:02:41 +0100

    CLOUD-1352 add #OUTPUT_VAR example

--- Example Task SSH.py
+++ Example Task SSH.py
@@ -1,28 +1,30 @@
-import re
-
-
 def handler(system, this):
     # Authenticate using private key
     info_task = this.task(
         'SSH',
         # public accessible name or IP
         hostname='my-ssh-server',
-        # key to check host identiy.
+        # key to check host identity.
         # can be read with "$ ssh-keyscan -t rsa <my-ssh-server>"
         hostkey='ssh-rsa AAAAB3NzaC1yc2E...',
         username='kevin',
         key='-----BEGIN RSA PRIVATE KEY-----\nMII...',
-        script='''
-               echo "hostname" "'$(hostname)'"
-               echo "username" "'$(id -un)'"
-               echo "cpu" "'$(uname -p)'"
-               '''
+        script=(
+            '''
+            HOSTNAME=$(hostname)
+            USERNAME=$(id -un)
+            CPU=$(uname -p)
+            #OUTPUT_VAR(HOSTNAME)
+            #OUTPUT_VAR(USERNAME)
+            #OUTPUT_VAR(CPU)
+            '''
+        ),
     ).run()
 
-    report = info_task.get('output_value')['report']
-    hostname = re.search("hostname '([^']*)'", report).group(1)
-    username = re.search("username '([^']*)'", report).group(1)
-    cpu = re.search("cpu '([^']*)'", report).group(1)
+    outputs = info_task.get('output_value')
+    hostname = outputs['var']['HOSTNAME']
+    username = outputs['var']['USERNAME']
+    cpu = outputs['var']['CPU']
 
     this.log(f'info_task was running on {hostname} using {cpu} as {username}')
 
@@ -33,12 +35,17 @@ def handler(system, this):
         hostkey='ssh-rsa AAAAB3NzaC1yc2E...',
         username='kevin',
         password='***',
-        script='''
-               echo "up since" "'$(uptime -s)'"
-               '''
+        script=(
+            '''
+            UPTIME=$(uptime -s)
+            #OUTPUT_VAR(UPTIME)
+            '''
+        ),
     ).run()
 
-    report = uptime_task.get('output_value')['report']
-    up_since = re.search("up since '([^']*)'", report).group(1)
+    outputs = uptime_task.get('output_value')
+    uptime = outputs['var']['UPTIME']
 
-    this.log(f'{hostname} is up since {up_since}')
+    this.log(f'{hostname} is up since {uptime}')
+
+    return this.success('all done')
