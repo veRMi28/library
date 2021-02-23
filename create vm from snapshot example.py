9453ceda37265afb291706a0e3eab3df1bdbb96f
michael <michael@cloudomation.com>
2021-02-23 13:10:56 +0100

    create VM on gcloud: requested changes

--- create vm from snapshot example.py
+++ create vm from snapshot example.py
@@ -13,8 +13,8 @@ def handler(system, this):
         change settings, anything you could do on a local machine in a terminal.  
 
         This flow also requires the two files:
-        * create vm from snapshot example.sh
-        * create vm from snapshot example.json
+        * create google cloud vm example.sh
+        * create google cloud vm example.json
         In the future, you can take these as an example, adopt them and 
         upload them with a new name in Cloudomation > Files.
     """
@@ -36,10 +36,10 @@ def handler(system, this):
                     'example': 'my-vm-on-gcloud',
                     'order': 1,
                 },
-                'project_name': {
+                'project_id': {
                     'element': 'string',
                     'type': 'string',
-                    'label': 'Enter the name of your project on google-cloud:',
+                    'label': 'Enter your google cloud project-ID:',
                     'default': 'my-first-project-id',
                     'order': 2,
                 },
@@ -47,7 +47,7 @@ def handler(system, this):
                     'element': 'string',
                     'type': 'string',
                     'label': 'Enter the machine type you want to use on google-cloud:',
-                    'default': 'n1-highcpu-4',
+                    'default': 'n1-standard-1',
                     'order': 3,
                 },
                 'server_location': {
@@ -66,14 +66,14 @@ def handler(system, this):
             },
             'required': [
                 'connection_name',
-                'project_name',
+                'project_id',
                 'machine_type',
                 'server_location',
             ],
         },
     ).wait().get('response')
     connection_name = form_response['connection_name']
-    project_id = form_response['project_name']
+    project_id = form_response['project_id']
     machine_type = form_response['machine_type']
     server_location = form_response['server_location']
 
@@ -85,41 +85,31 @@ def handler(system, this):
 
     # load the startup script and pass the public ssh key to the script: 
     vm_startup_script = system.file(
-        'create vm from snapshot example.sh').get('content')
+        'create google cloud vm example.sh').get('content')
     vm_startup_script = vm_startup_script.format(
         setup_user_pub=setup_user_pub,
     )
 
     # load the configuration for the VM and setup last parameters:
     vm_config_str = system.file(
-        'create vm from snapshot example.json').get('content')
+        'create google cloud vm example.json').get('content')
     vm_config = json.loads(vm_config_str)
-    vm_config['name'] = f'{connection_name}'
+    vm_config['name'] = connection_name
     vm_config['machineType'] = \
         f'projects/{project_id}/zones/{server_location}/machineTypes/{machine_type}'
     vm_config['zone'] = f'projects/{project_id}/zones/{server_location}'
     vm_config['metadata']['items'][0]['value'] = vm_startup_script
-    vm_config['disks'][0]['deviceName'] = f'{connection_name}'
-    vm_config['disks'][0]['diskType'] = \
+    vm_config['disks'][0]['deviceName'] = connection_name
+    vm_config['disks'][0]['initializeParams']['diskType'] = \
         f'projects/{project_id}/zones/{server_location}/diskTypes/pd-ssd'
     vm_config['networkInterfaces'][0]['subnetwork'] = \
-        f'projects/{project_id}/regions/europe-west1/subnetworks/default'
+        f'projects/{project_id}/regions/{server_location[:-2]}/subnetworks/default'
     # NOTE: two sources can be used for initial system installation:
     # a) from 'sourceSnapshot'
     # b) from 'sourceImage'
-    # In the example .json the parameter 'sourceSnapshot' is defined. 
-    # This flow script shall install Ubuntu which is provided from a 'sourceImage'.
-    # Since only either of the two options can be given as a parameter, 
-    # the paramter 'sourceSnapshot' must be removed 
-    # (either here, see below, or in the .json).
-    find_image_name = this.flow('find build-server image name', wait=False)
-    image_name = find_image_name.wait().get('output_value')['image_name']
-    vm_config['disks'][0]['initializeParams'][
-        'sourceImage'] = f'projects/ubuntu-os-cloud/global/images/{image_name}'
-    try:
-        del vm_config['disks'][0]['initializeParams']['sourceSnapshot']
-    except KeyError:
-        pass
+    # This flow script will install Ubuntu, using the parameter 'sourceImage'.
+    # For installing a system from snapshot use the respective parameter.
+    # This paramter is set under: disks / initializeParams
         
     # Connect to the VM
     operation = this.connect(
@@ -140,9 +130,9 @@ def handler(system, this):
         operation=operation,
     )
 
-    try:
-        # retrieve external IP and hostkey (required for creating a SSH connection):
-        for _ in range(60):
+    # retrieve external IP and hostkey (required for creating a SSH connection):
+    for _ in range(60):
+        try:
             instance_list = this.connect(
                 'gcloud',
                 name=f'find {connection_name}',
@@ -156,17 +146,21 @@ def handler(system, this):
                     'filter': f'name={connection_name}',
                 },
             ).get('output_value')
-            if len(instance_list['result'].get('items', [])) != 0:
-                break
+        except flow_api.DependencyFailedError:
             this.sleep(1)
-        else:
-            return this.error('did not find VM within 60 tries')
-        external_ip = instance_list['result']['items'][0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+        if len(instance_list['result'].get('items', [])) != 0:
+            break
+        this.sleep(1)
+    else:
+        return this.error('did not find VM within 60 tries')
+    external_ip = instance_list['result']['items'][0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']
 
-        failed_tries = []
-        for _ in range(60):
-            this.sleep(1)
-            guest_attribute_task = this.connect(
+    last_try_guest_attribute = None
+    for _ in range(60):
+        if last_try_guest_attribute is not None:
+            last_try_guest_attribute.archive()
+        try:
+            last_try_guest_attribute = this.connect(
                 'gcloud',
                 name=f'get {connection_name} hostkey',
                 api_name='compute',
@@ -176,39 +170,40 @@ def handler(system, this):
                 params={
                     'project': project_id,
                     'zone': server_location,
-                    'instance': f'{connection_name}',
+                    'instance': connection_name,
                     'queryPath': 'hostkeys/ssh-rsa',
                 },
                 wait=system.return_when.ALL_ENDED,
             )
-            if guest_attribute_task.load('status') == 'ENDED_SUCCESS':
-                break
-            failed_tries.append(guest_attribute_task)
+        except Exception:
+            this.sleep(1)
         else:
-            return this.error('did not find hostkey within 60 tries')
-        for failed_try in failed_tries:
-            failed_try.archive()
-        hostkey = guest_attribute_task.get('output_value')[
-            'result']['queryValue']['items'][0]['value']
+            if last_try_guest_attribute.load('status') == 'ENDED_SUCCESS':
+                break
+    else:
+        return this.error('did not find hostkey within 60 tries')
+    hostkey = last_try_guest_attribute.get('output_value')['result']['queryValue']['items'][0]['value']
 
-        # setup the SSH connection for the VM
-        system.connection(f'{connection_name}-ssh').save(
-            connection_type='SSH',
-            value={
-                'hostname': external_ip,
-                'hostkey': hostkey,
-                'username': 'setup-user',
-                'key': setup_user_priv,
-                'script_timeout': 600,
-                'interpreter': '/usr/bin/env bash -ex',
-            },
-        )
-        this.sleep(15)
+    # setup the SSH connection for the VM
+    system.connection(f'{connection_name}-ssh').save(
+        connection_type='SSH',
+        value={
+            'hostname': external_ip,
+            'hostkey': hostkey,
+            'username': 'setup-user',
+            'key': setup_user_priv,
+            'script_timeout': 600,
+            'interpreter': '/usr/bin/env bash -ex',
+        },
+    )
 
-        # Run a little bash script on the VM:
-        script_echo = dict(result='')
+    # Run a little bash script on the VM:
+    last_try_script_echo = None
+    for _ in range(20):
+        if last_try_script_echo is not None:
+            last_try_script_echo.archive()
         try:
-            script_echo = this.connect(
+            last_try_script_echo = this.connect(
                 f'{connection_name}-ssh',
                 script="""
                     echo "litte test content" >> ~/test_file.txt
@@ -216,48 +211,38 @@ def handler(system, this):
                     rm ~/test_file.txt
                     """,
                 name='run test script on new vm',
-            ).get('output_value')['report']
-        except Exception as err:
-            return this.error(f'Failed to run the little test script. \n{repr(err)}')
+            )
+        except:
+            this.sleep(1)
         else:
-            # this.set_output(f'Echo from test-script:\n{script_echo['result']}')
-            this.set_output(f'Echo from test-script:\n{script_echo}')
+            if last_try_script_echo.load('status') == 'ENDED_SUCCESS':
+                break
+    else:
+        return this.error('Failed to run the little test script.')
+    script_echo = last_try_script_echo.get('output_value')['report']
+    this.set_output(f'Echo from test-script:\n{script_echo}')
 
-        # Example: Stop the VM
-        operation = this.connect(
-            'gcloud',
-            name=f'stop {connection_name}',
-            api_name='compute',
-            api_version='v1',
-            collection='instances',
-            request='stop',
-            params={
-                'project': project_id,
-                'zone': server_location,
-                'instance': f'{connection_name}',
-            },
-        ).get('output_value')['result']
-        this.flow(
-            'google_operation_wait',
-            operation=operation,
-        )
+    # Save flow state and pause, manually resume this script's execution to remove the VM
+    this.save(message='The VM was successfully deployed. Resume this execution to remove the VM')
+    this.pause()
 
-    except Exception as ex:
-        this.save(message=repr(ex))
-        raise
-    finally:
-        instance_list = this.connect(
-            'gcloud',
-            name=f'find {connection_name}',
-            api_name='compute',
-            api_version='v1',
-            collection='instances',
-            request='list',
-            params={
-                'project': project_id,
-                'zone': server_location,
-                'filter': f'name={connection_name}',
-            },
-        ).get('output_value')
-        this.set_output(f'Found instances of this VM:\n{instance_list}')
+    # Remove the VM
+    operation = this.connect(
+        'gcloud',
+        name=f'remove {connection_name}',
+        api_name='compute',
+        api_version='v1',
+        collection='instances',
+        request='delete',
+        params={
+            'project': project_id,
+            'zone': server_location,
+            'instance': connection_name,
+        },
+    ).get('output_value')['result']
+    this.flow(
+        'google_operation_wait',
+        operation=operation,
+    )
+
     return this.success('all done')
