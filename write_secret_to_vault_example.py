6dba2c85507b4731e9b4c7a8af1a1ea114aaa53c
veRMi28 <74329830+veRMi28@users.noreply.github.com>
2021-03-23 12:27:41 +0100

    Create write_secret_to_vault_example.py
    related to ticket CLOUD-6005

---
+++ write_secret_to_vault_example.py
@@ -0,0 +1,23 @@
+import flow_api
+
+def handler(system: flow_api.System, this: flow_api.Execution):
+    # assuming vault API version 2 for KV
+    target_vault = 'default_local' # if None, then is iterating over all vaults in DB
+    path = 'abcd-test'
+    my_secrets = {
+        "password": "my-password",
+        "username": "abcd@cloudomation.com",
+    }
+    # my_secrets = {
+    #     "data": {
+    #         "password": "JKF8z9fesehGFW7e",
+    #         "username": "michael@cloudomation.com",
+    #     }
+    # }
+    
+    my_vault = system.vault_config(target_vault)
+    my_vault.write_secret(
+        path,
+        my_secrets,
+    )
+    return this.success('all done')
